658877e565df8952fca7c34437cb5004ec7f5ad1
lg <lg4869@outlook.com>
2024-02-08 17:59:56 +0000

    build mpi

--- tests/dist/mpi/native/main.cpp
+++ tests/dist/mpi/native/main.cpp
@@ -1,7 +1,13 @@
-int bench_all_reduce();
+namespace tests::mpi {
+int bench_allreduce();
+}
 
 int main(int argc, char* argv[])
 {
-    bench_all_reduce();
+    tests::mpi::bench_allreduce();
     return 0;
 }
+
+/*
+docker run --user mpirun --rm -it --entrypoint bash faasm.azurecr.io/openmpi-worker:0.13.1
+*/
