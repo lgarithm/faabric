63956aecd3b19da780869edf1f8adfd7d3267d9f
lg <lg4869@outlook.com>
2024-03-01 16:48:24 +0000

    upload results

--- comment.sh
+++ comment.sh
@@ -1,16 +1,49 @@
 #!/bin/sh
 
+down() {
+    local commit=$1
+    echo "commit: $commit"
+    local d=bench/$commit/faabric
+    mkdir -p $commit
+    scp ko3:$d/1.txt "$commit/1.txt"
+    scp ko3:$d/2.txt "$commit/2.txt"
+}
+
 cmt() {
     gh pr comment 2 --body "$1" --repo https://github.com/lgarithm/faabric
 }
 
 body() {
-    echo "$1"
+    local commit=$1
+    echo "$commit"
+
     echo
     echo '```'
-    cat "$1"
+    cat "$commit/1.txt"
+    echo '```'
+
+    echo '```'
+    cat "$commit/2.txt"
     echo '```'
 }
 
+f() {
+    local commit=$1
+    echo $commit
+    down $commit
+    cmt $(body $commit)
+}
+
+main() {
+    local ids=$1
+    for id in $(cat $ids | awk '{print $1}'); do
+        echo "commit: $id"
+        f $id
+        echo "done for $id"
+        echo
+    done
+}
+
+main ids.txt
 # cmt "$(body "7483943ede55cb90394eedaa670ec169239eeb0c.log")"
-cmt "$(body "8d10fa2051ff19cb141515296874d7d2bf2f3235.log")"
+# cmt "$(body "8d10fa2051ff19cb141515296874d7d2bf2f3235.log")"
